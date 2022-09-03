4f1a8a3bd6dec2e4499f02134f3e541e63b27362
John Oram <john@oram.ca>
2022-09-02 21:03:08 -0700

    can select polygon

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, {Component, useState} from 'react';
+import { DrawingManager, GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: 49.1026,
+  lng: -123.4753
+};
+
+
+
+
+function Map(props) {
+    let {polygon, setPolygon} = useState([]);
+
+    const onPolygonComplete = polygon => {
+        console.log(polygon.getPath().getArray());
+    }
+
+    return (
+        <LoadScript googleMapsApiKey="AIzaSyANDvIT7YDXDjP-LW0bFRdoFwm9QeL9q1g" libraries={['drawing']} >
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={8} >
+                <DrawingManager onPolygonComplete={onPolygonComplete} />
+            </GoogleMap>
+        </LoadScript>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Map/>
+        </div>
+    );
 }
 
 export default App;
